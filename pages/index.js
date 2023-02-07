e6f17249bef798471ef879917779cfc47bf6059c
Afiyaayat <37040878+sohelranacse@users.noreply.github.com>
2023-02-07 17:44:30 +0600

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from "next/head";
 
 export default function Home() {
+
   return (
     <>
       <Head>
-        <title>GCC Limited</title>
-        <meta name="description" content="Find out about GCC&#039;s certified IT Support &amp; Consultancy, Software Solutions and Cloud services in London, Gloucester, Birmingham &amp; The West Midlands" />
+        <title>Learn Programming</title>
+        <meta name="description" content="Learn Programming" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
