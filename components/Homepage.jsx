e715d0a13552b7bd4b6cc0a6d34876163cd90f4e
Afiyaayat <37040878+sohelranacse@users.noreply.github.com>
2023-02-13 16:08:50 +0600

    update

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Note from "./common/Note";
 
 function Homepage() {
   return (
     <div className="container mx-auto px-2 h-screen mt-14">
       <p>Good</p>
+      <Note />
     </div>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
         <Html>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin='true' />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
                 <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
             </Head>
             <body>
